5b8cfcf7a8d807a2cf3bcbb7ecfd23c248bb0d06
akulanga <akulanga@COMPSCI-FAC>
2026-04-21 06:38:04 -0500

    Edited by Ashwin

--- Damian_lab1/Damian_lab1.cpp
+++ Damian_lab1/Damian_lab1.cpp
@@ -1,8 +1,10 @@
 //edited directly on GitHub
+//edited by Ashwin on VS
 #include <iostream>
 using namespace std;
 int main()
 {
+    cout << "Hi from Ashwin!" << endl;
     cout << "Hello from La'Damian! This is my first Git commit.\n";
     cout << "Git makes collaboration easy!" << endl;
 }
