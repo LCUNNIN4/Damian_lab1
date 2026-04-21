f2a3124142fb46368960bc8258d37f6b0d142e62
ladamian.cunningham1980@gmail.com <ladamian.cunningham1980@gmail.com>
2026-04-21 11:30:49 -0500

    Final mini-task update.

--- Damian_lab1/Damian_lab1.cpp
+++ Damian_lab1/Damian_lab1.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Hi from Ashwin!" << endl;
     cout << "Hello from La'Damian! This is my first Git commit.\n";
     cout << "Git makes collaboration easy!" << endl;
+    cout << "Lab 1 completed on 4/21/2026!" << endl;
 }
 
